49f147053dc2ee25e9c411e2f7d437897ac774fa
yyc <yycnormal@163.com>
2022-06-02 10:03:59 +0800

    创建Hello.java

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @author yyc
+ * @description
+ * @create 2022/6/2 10:01
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+
+    }
+}
